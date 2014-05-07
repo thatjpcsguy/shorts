d93c6ac337f081c1c822a0c302f57cd6a62d4f73
James Cooper-Stanbury <thatguy@jpcs.me>
2014-05-07 13:39:12 +1000

    basic ajax shit

--- www/js/shorts.js
+++ www/js/shorts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+	console.log("ready");
     $.get('/ajax/classify.php', {}, function(data){
         $('#prediction-container').html('<h1>'+data['prediction']+'</h1>');
         $('#location-container').html('<h4>'+data['location']+'</h4>');
