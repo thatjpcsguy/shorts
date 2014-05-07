2991c58c96476f640544b1adec1afe990ad68430
James Cooper-Stanbury <thatguy@jpcs.me>
2014-05-07 15:28:03 +1000

    added some sort of api

--- www/ajax/classify.php
+++ www/ajax/classify.php
@@ -1,10 +1,13 @@
 <?php
 
-$location = $_GET['location'];
+$lat = $_GET['lat'];
+$lon = $_GET['lon'];
+
+$res = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/forecast/daily?lat=".$lat."&lon=".$lon."&mode=json"));
 
 header("Content-type: text/json");
 
-$ret = array('location' => 'Sydney, Australia', 'prediction' => 'Today is one hell of a shorts day!');
+$ret = array('location' => 'Sydney, Australia', 'prediction' => 'Today is one hell of a shorts day!', 'res' => $res);
 
 echo json_encode($ret);
 
--- www/js/shorts.js
+++ www/js/shorts.js
@@ -1,10 +1,48 @@
+
+
 $(document).ready(function(){
-	console.log("ready");
-    $.get('/ajax/classify.php', {}, function(data){
-        $('#prediction-container').html('<h1>'+data['prediction']+'</h1>');
-        $('#location-container').html('<h4>'+data['location']+'</h4>');
+	function showPosition(position)
+	{
+		console.log("lat: "+position.coords.latitude);
+		console.log("lon: "+position.coords.longitude);
 
-    });
+	    $.get('/ajax/classify.php', {lat: position.coords.latitude, lon: position.coords.longitude}, function(data){
+	    	console.log(data['res']);
+	        $('#prediction-container').html('<h1>'+data['prediction']+'</h1>');
+	        $('#location-container').html('<h4>'+data['location']+'</h4>');
 
+	    });
+
+	}
+
+	function showError(error)
+	{
+		switch(error.code) 
+		{
+			case error.PERMISSION_DENIED:
+				console.log("User denied the request for Geolocation.");
+				break;
+			case error.POSITION_UNAVAILABLE:
+				console.log("Location information is unavailable.");
+				break;
+			case error.TIMEOUT:
+				console.log("The request to get user location timed out.");
+				break;
+			case error.UNKNOWN_ERROR:
+				console.log("An unknown error occurred.");
+				break;
+		}
+	}
+
+	console.log("ready")
+
+	if (navigator.geolocation)
+    {
+    	console.log("HTML Location Available.");
+    	navigator.geolocation.getCurrentPosition(showPosition, showError);
+    }
+	else {
+		console.log('HTML Location Not Supported.');
+	}
 
 });
