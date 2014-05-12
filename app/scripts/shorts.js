0e6ebb5b615891ea8870ca880287bcdd4a6ae8a3
James Cooper-Stanbury <thatguy@jpcs.me>
2014-05-12 10:25:53 +1000

    made some small changes

--- app/scripts/shorts.js
+++ app/scripts/shorts.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     function showPosition(position) {
-        console.log("lat: " + position.coords.latitude);
-        console.log("lon: " + position.coords.longitude);
+        console.log('lat: ' + position.coords.latitude);
+        console.log('lon: ' + position.coords.longitude);
 
         $.get('/ajax/classify.php', {
             lat: position.coords.latitude,
             lon: position.coords.longitude
         }, function(data) {
-            console.log(data['res']);
+            console.log(data.res);
 
             $('#weather-container').html('<h4>' + kelvinToCelcius(data.res.list[0].temp.day) + ' degrees celcius </h4>');
-            $('#prediction-container').html('<h1>' + data['prediction'] + '</h1>');
-            $('#location-container').html('<h4>' + data['location'] + '</h4>');
+            $('#prediction-container').html('<h1>' + data.prediction + '</h1>');
+            $('#location-container').html('<h4>' + data.location + '</h4>');
 
         });
 
@@ -28,22 +28,23 @@ $(document).ready(function() {
     function showError(error) {
         switch (error.code) {
             case error.PERMISSION_DENIED:
-                console.log("User denied the request for Geolocation.");
+                console.log('User denied the request for Geolocation.');
                 break;
             case error.POSITION_UNAVAILABLE:
-                console.log("Location information is unavailable.");
+                console.log('Location information is unavailable.');
                 break;
             case error.TIMEOUT:
-                console.log("The request to get user location timed out.");
+                console.log('The request to get user location timed out.');
                 break;
             case error.UNKNOWN_ERROR:
-                console.log("An unknown error occurred.");
+                console.log('An unknown error occurred.');
                 break;
         }
+        // fall back to geo ip
     }
 
     if (navigator.geolocation) {
-        console.log("HTML Location Available.");
+        console.log('HTML Location Available.');
         navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
         console.log('HTML Location Not Supported.');
